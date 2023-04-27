bd6bb6611819eee946520be804e41eaca504c3e5
tushar24081 <tushar24081@gmail.com>
2023-04-27 11:10:41 +0530

    Sorting on Grouped Data

--- src/Pages/List/List.js
+++ src/Pages/List/List.js
@@ -11,34 +11,64 @@ import VerticalAlignCenterIcon from '@mui/icons-material/VerticalAlignCenter';
 import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
 import { Link } from 'react-router-dom';
 import { Navbar } from '../Home/Navbar';
+import { addIfDoesntExists } from '../../Services/localstorage.service';
 export const List = () => {
   let allData = loadDataFromLocal();
   let [dataValue, setDataValue] = useState(allData);
-  const [sortedOrder, setSortedOrder] = useState("asc");
-  let [groupedData, setGroupedData] = useState([]);
-  let handleSort = (title) => {
-  let sortedData;
-    console.log(title);
-    if (title === "amount") {
-      sortedData = [...dataValue].sort((d1, d2) => {
-        if (sortedOrder === "asc") {
-          return d1[title] - d2[title];
-        } else {
-          return d2[title] - d1[title];
-        }
-      });
+  addIfDoesntExists()
+  const [sortedOrder, setSortedOrder] = useState("desc");
+  let [groupedData, setGroupedData] = useState({});
+  let handleSort = (title, grid = false, data = "") => {
+    let sortedData;
+    let groupSortData;
+    if (!grid) {
+      if (title === "amount") {
+        sortedData = [...dataValue].sort((d1, d2) => {
+          if (sortedOrder === "asc") {
+            return d1[title] - d2[title];
+          } else {
+            return d2[title] - d1[title];
+          }
+        });
+      } else {
+        sortedData = [...dataValue].sort((d1, d2) => {
+          if (sortedOrder === "asc") {
+            return d1[title].localeCompare(d2[title])
+          } else {
+            return d2[title].localeCompare(d1[title])
+          }
+        });
+      }
+      setSortedOrder(sortedOrder === "asc" ? "desc" : "asc");
+      setDataValue(sortedData);
     } else {
-      sortedData = [...dataValue].sort((d1, d2) => {
-        if (sortedOrder === "asc") {
-          return d1[title].localeCompare(d2[title])
-        } else {
-          return d2[title].localeCompare(d1[title])
-        }
-      });
+      console.log("Executing Grid One");
+      let objToUpdate = groupedData[data];
+      if (title === "amount") {
+        groupSortData = [...objToUpdate].sort((d1, d2) => {
+          if (sortedOrder === "asc") {
+            return d1[title] - d2[title];
+          } else {
+            return d2[title] - d1[title];
+          }
+        });
+      } else {
+        groupSortData = [...objToUpdate].sort((d1, d2) => {
+          if (sortedOrder === "asc") {
+            return d1[title].localeCompare(d2[title])
+          } else {
+            return d2[title].localeCompare(d1[title])
+          }
+        })
+      }
+
+      setSortedOrder(sortedOrder === "asc" ? "desc" : "asc");
+      groupedData[data] = groupSortData;
+      setGroupedData(groupedData)
     }
 
-    setSortedOrder(sortedOrder === "asc" ? "desc" : "asc");
-    setDataValue(sortedData);
+
+
   }
   var searchingValues;
   const handleChange = (e) => {
@@ -47,32 +77,34 @@ export const List = () => {
       return data[search]
     })
     searchingValues = [...new Set(searchingValues)]
-    let answer = searchingValues.map((searchValue) => (
-      allData.filter((data) => {
+    const answerObj = {}
+    searchingValues.forEach((searchValue) => {
+      answerObj[searchValue] = allData.filter((data) => {
         return data[search] === searchValue
       })
-    ))
+    })
+    console.log(answerObj);
 
     setDataValue([])
-    setGroupedData(answer);
+    setGroupedData(answerObj);
   }
   const handleRemoveFilter = () => {
     setDataValue(allData);
-    setGroupedData([])
+    setGroupedData({})
   }
   return (
     <>
       <Navbar />
-      <select onChange={handleChange}>
-        <option disabled selected>Select Group By</option>
+      <select onChange={handleChange} defaultValue={"basic"}>
+        <option disabled value="basic">Select Group By</option>
         <option value="month">Month</option>
         <option value="year">Year</option>
         <option value="transaction">Transaction</option>
         <option value="from_account">From Account</option>
         <option value="to_account">To Account</option>
       </select>
-      {groupedData.length > 0 && <button onClick={handleRemoveFilter}>Remove Filter</button>}
-      
+      {Object.keys(groupedData).length > 0 && <button onClick={handleRemoveFilter}>Remove Filter</button>}
+
       {dataValue.length > 0 && <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead style={{ background: "rgb(160, 162, 192)", color: "white", cursor: "pointer" }}>
@@ -89,7 +121,7 @@ export const List = () => {
             </TableRow>
           </TableHead >
           <TableBody>
-            {dataValue.map((data, index) => (
+            {dataValue.length > 0 && dataValue.map((data, index) => (
               <TableRow
                 key={index}
                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
@@ -112,40 +144,42 @@ export const List = () => {
           </TableBody>
         </Table >
       </TableContainer >}
-
-      {groupedData.length > 0 && groupedData.map((singleData, index) => (
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead style={{ background: "rgb(160, 162, 192)", color: "white", cursor: "pointer" }}>
-            <TableRow>
-              <TableCell onClick={() => handleSort("month")}> Month <VerticalAlignCenterIcon /></TableCell>
-              <TableCell align="right" onClick={() => handleSort("year")}>Year <VerticalAlignCenterIcon /></TableCell>
-              <TableCell align="right" onClick={() => handleSort("transaction")}>Transaction <VerticalAlignCenterIcon /></TableCell>
-              <TableCell align="right" onClick={() => handleSort("from_account")}>From Account <VerticalAlignCenterIcon /></TableCell>
-              <TableCell align="right" onClick={() => handleSort("to_account")}>To Account <VerticalAlignCenterIcon /></TableCell>
-              <TableCell align="right" onClick={() => handleSort("amount")}>Amount <VerticalAlignCenterIcon /></TableCell>
-              <TableCell align="right">Image <VerticalAlignCenterIcon /></TableCell>
-            </TableRow>
-          </TableHead >
-          <TableBody>
-            {singleData.map((d, index) => (
-              <TableRow
-                key={index}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                <TableCell component="th" scope="row">
-                  {d.month}
-                </TableCell>
-                <TableCell align="right">{d.year}</TableCell>
-                <TableCell align="right">{d.transaction}</TableCell>
-                <TableCell align="right">{d.from_account}</TableCell>
-                <TableCell align="right">{d.to_account}</TableCell>
-                <TableCell align="right">{d.currency}{d.amount}</TableCell>
-                <TableCell align="right">
-                  <img src={d.selectedFile} alt='error in loading' width={75} height={75} />
-                </TableCell>
+      {Object.keys(groupedData) && Object.keys(groupedData).map((data, index) => (
+        <div>
+          <h2>{data}</h2>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead style={{ background: "rgb(160, 162, 192)", color: "white", cursor: "pointer" }}>
+              <TableRow>
+                <TableCell onClick={() => handleSort("month", true, data)}> Month <VerticalAlignCenterIcon /></TableCell>
+                <TableCell align="right" onClick={() => handleSort("year", true, data)}>Year <VerticalAlignCenterIcon /></TableCell>
+                <TableCell align="right" onClick={() => handleSort("transaction", true, data)}>Transaction <VerticalAlignCenterIcon /></TableCell>
+                <TableCell align="right" onClick={() => handleSort("from_account", true, data)}>From Account <VerticalAlignCenterIcon /></TableCell>
+                <TableCell align="right" onClick={() => handleSort("to_account", true, data)}>To Account <VerticalAlignCenterIcon /></TableCell>
+                <TableCell align="right" onClick={() => handleSort("amount", true, data)}>Amount <VerticalAlignCenterIcon /></TableCell>
+                <TableCell align="right">Image <VerticalAlignCenterIcon /></TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+            </TableHead >
+            <TableBody>
+              {groupedData[data].map((d, index) => (
+                <TableRow
+                  key={index}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                  <TableCell component="th" scope="row">
+                    {d.month}
+                  </TableCell>
+                  <TableCell align="right">{d.year}</TableCell>
+                  <TableCell align="right">{d.transaction}</TableCell>
+                  <TableCell align="right">{d.from_account}</TableCell>
+                  <TableCell align="right">{d.to_account}</TableCell>
+                  <TableCell align="right">{d.currency}{d.amount.toLocaleString("en-IN")}</TableCell>
+                  <TableCell align="right">
+                    <img src={d.selectedFile} alt='error in loading' width={75} height={75} />
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
       ))}
 
 
--- src/Pages/List/backup.jsx
+++ src/Pages/List/backup.jsx
@@ -1,13 +1,34 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { addIfDoesntExists } from '../../Services/localstorage.service'
-export const Home = () => {
-  addIfDoesntExists()
-  return (
-    <>
-        <Link to="/add">Add Expense</Link>
-        <br />
-        <Link to="/show">Check All Data</Link>
-    </>
-  )
-}
+{groupedData.length > 0 && groupedData.map((singleData, index) => (
+  <Table sx={{ minWidth: 650 }} aria-label="simple table">
+    <TableHead style={{ background: "rgb(160, 162, 192)", color: "white", cursor: "pointer" }}>
+      <TableRow>
+        <TableCell onClick={() => handleSort("month", true)}> Month <VerticalAlignCenterIcon /></TableCell>
+        <TableCell align="right" onClick={() => handleSort("year", true)}>Year <VerticalAlignCenterIcon /></TableCell>
+        <TableCell align="right" onClick={() => handleSort("transaction", true)}>Transaction <VerticalAlignCenterIcon /></TableCell>
+        <TableCell align="right" onClick={() => handleSort("from_account", true)}>From Account <VerticalAlignCenterIcon /></TableCell>
+        <TableCell align="right" onClick={() => handleSort("to_account", true)}>To Account <VerticalAlignCenterIcon /></TableCell>
+        <TableCell align="right" onClick={() => handleSort("amount", true)}>Amount <VerticalAlignCenterIcon /></TableCell>
+        <TableCell align="right">Image <VerticalAlignCenterIcon /></TableCell>
+      </TableRow>
+    </TableHead >
+    <TableBody>
+      {singleData.map((d, index) => (
+        <TableRow
+          key={index}
+          sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+          <TableCell component="th" scope="row">
+            {d.month}
+          </TableCell>
+          <TableCell align="right">{d.year}</TableCell>
+          <TableCell align="right">{d.transaction}</TableCell>
+          <TableCell align="right">{d.from_account}</TableCell>
+          <TableCell align="right">{d.to_account}</TableCell>
+          <TableCell align="right">{d.currency}{d.amount}</TableCell>
+          <TableCell align="right">
+            <img src={d.selectedFile} alt='error in loading' width={75} height={75} />
+          </TableCell>
+        </TableRow>
+      ))}
+    </TableBody>
+  </Table>
+))}
